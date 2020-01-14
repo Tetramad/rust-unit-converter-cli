e2f9d3425874e253cd40228d0ac9f64d23f7e365
Tetramad <cbwjd777@gmail.com>
2020-01-14 17:17:41 +0900

    feat: 단위 변환 가능 확인 구현

--- src/unit.rs
+++ src/unit.rs
@@ -1,17 +1,22 @@
+pub enum Unit {
+    Area,
+    Length,
+    Volume,
+}
+
 pub fn identify(strings: &(String, String, String)) -> (Unit, Unit, ()) {
     unimplemented!();
 }
 
 pub fn is_convertable(from: &Unit, to: &Unit) -> bool {
-    unimplemented!();
+    match (from, to) {
+        (Unit::Area, Unit::Area) | (Unit::Length, Unit::Length) | (Unit::Volume, Unit::Volume) => {
+            true
+        }
+        _ => false,
+    }
 }
 
 pub fn convert(from: &Unit, to: &Unit, value: ()) -> f64 {
     unimplemented!();
-}
-
-pub enum Unit {
-    Area,
-    Length,
-    Volume,
 }
