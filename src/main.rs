b2866fcf9f98bb552723f16ef92004b0a140e262
Tetramad <cbwjd777@gmail.com>
2020-01-15 23:37:52 +0900

    feat: 에러 처리

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,37 @@ fn main() {
     let args = arguments();
     let (from, to, value) = identify(&args);
 
-    println!("{}", convert(&from, &to, value).unwrap());
+    match convert(&from, &to, value) {
+        Ok(converted) => {
+            println!("{}", converted);
+            std::process::exit(0);
+        }
+        Err(message) => {
+            eprintln!("{}", message);
+            std::process::exit(1);
+        }
+    }
 }
 
 fn identify(args: &(String, String, String)) -> (UnitLiteral, UnitLiteral, f64) {
+    let on_failure = |m: String| {
+        eprintln!("{}", m);
+        std::process::exit(1);
+    };
+
     match (
         args.0.parse::<f64>(),
         args.1.parse::<f64>(),
         args.2.parse::<f64>(),
     ) {
         (Ok(value), Err(_), Err(_)) => (
-            UnitLiteral::try_from(&args.1).unwrap(),
-            UnitLiteral::try_from(&args.2).unwrap(),
+            UnitLiteral::try_from(&args.1).unwrap_or_else(on_failure),
+            UnitLiteral::try_from(&args.2).unwrap_or_else(on_failure),
             value,
         ),
         (Err(_), Err(_), Ok(value)) => (
-            UnitLiteral::try_from(&args.0).unwrap(),
-            UnitLiteral::try_from(&args.1).unwrap(),
+            UnitLiteral::try_from(&args.0).unwrap_or_else(on_failure),
+            UnitLiteral::try_from(&args.1).unwrap_or_else(on_failure),
             value,
         ),
         _ => unimplemented!(),
