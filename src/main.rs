76479dd1f4dd3147d7c5e6cb5dc3139334be7d8e
Tetramad <cbwjd777@gmail.com>
2020-01-15 18:51:09 +0900

    feat: 단위 체크 구현

--- src/main.rs
+++ src/main.rs
@@ -5,95 +5,93 @@ fn main() {
     let args = argparser::arguments();
     let (from, to, value) = unit::identify(&args);
 
-    if !unit::is_convertable(&from, &to) {
+    if unit::is_convertable(&from, &to) {
+        println!("{}", unit::convert(&from, &to, value));
+    } else {
         panic!();
     }
-
-    let value = unit::convert(&from, &to, value);
-
-    println!("{}", value);
 }
 
-    /*
-    if from.as_str() == "m" || from.as_str() == "yd" || from.as_str() == "리" {
-        let value = match from.as_str() {
-            "m" => length::Length::Meter(value),
-            "yd" => length::Length::Yard(value),
-            "리" => length::Length::Ri(value),
-            _ => panic!(),
-        };
+/*
+if from.as_str() == "m" || from.as_str() == "yd" || from.as_str() == "리" {
+    let value = match from.as_str() {
+        "m" => length::Length::Meter(value),
+        "yd" => length::Length::Yard(value),
+        "리" => length::Length::Ri(value),
+        _ => panic!(),
+    };
 
-        let converted = match to.as_str() {
-            "m" => length::conv_to_meter(value),
-            "yd" => length::conv_to_yard(value),
-            "리" => length::conv_to_ri(value),
-            _ => panic!(),
-        };
+    let converted = match to.as_str() {
+        "m" => length::conv_to_meter(value),
+        "yd" => length::conv_to_yard(value),
+        "리" => length::conv_to_ri(value),
+        _ => panic!(),
+    };
 
-        let v = match converted {
-            length::Length::Meter(m) => m,
-            length::Length::Ri(r) => r,
-            length::Length::Yard(y) => y,
-        };
+    let v = match converted {
+        length::Length::Meter(m) => m,
+        length::Length::Ri(r) => r,
+        length::Length::Yard(y) => y,
+    };
 
-        println!("{}", v);
-    } else if from.as_str() == "m2"
-        || from.as_str() == "ha"
-        || from.as_str() == "ac"
-        || from.as_str() == "평"
-    {
-        let value = match from.as_str() {
-            "m2" => area::Area::SqrMeter(value),
-            "ha" => area::Area::Hactare(value),
-            "ac" => area::Area::Acre(value),
-            "평" => area::Area::Pyeong(value),
-            _ => panic!(),
-        };
+    println!("{}", v);
+} else if from.as_str() == "m2"
+    || from.as_str() == "ha"
+    || from.as_str() == "ac"
+    || from.as_str() == "평"
+{
+    let value = match from.as_str() {
+        "m2" => area::Area::SqrMeter(value),
+        "ha" => area::Area::Hactare(value),
+        "ac" => area::Area::Acre(value),
+        "평" => area::Area::Pyeong(value),
+        _ => panic!(),
+    };
 
-        let converted = match to.as_str() {
-            "m2" => area::conv_to_sqr_meter(value),
-            "ha" => area::conv_to_hactare(value),
-            "평" => area::conv_to_pyeong(value),
-            "ac" => area::conv_to_acre(value),
-            _ => panic!(),
-        };
+    let converted = match to.as_str() {
+        "m2" => area::conv_to_sqr_meter(value),
+        "ha" => area::conv_to_hactare(value),
+        "평" => area::conv_to_pyeong(value),
+        "ac" => area::conv_to_acre(value),
+        _ => panic!(),
+    };
 
-        let v = match converted {
-            area::Area::SqrMeter(m2) => m2,
-            area::Area::Hactare(ha) => ha,
-            area::Area::Pyeong(pyeong) => pyeong,
-            area::Area::Acre(ac) => ac,
-        };
+    let v = match converted {
+        area::Area::SqrMeter(m2) => m2,
+        area::Area::Hactare(ha) => ha,
+        area::Area::Pyeong(pyeong) => pyeong,
+        area::Area::Acre(ac) => ac,
+    };
 
-        println!("{}", v);
-    } else if from.as_str() == "L"
-        || from.as_str() == "gal"
-        || from.as_str() == "oz"
-        || from.as_str() == "되"
-    {
-        let value = match from.as_str() {
-            "L" => volume::Volume::Liter(value),
-            "gal" => volume::Volume::Gallon(value),
-            "oz" => volume::Volume::Ounce(value),
-            "되" => volume::Volume::Doe(value),
-            _ => panic!(),
-        };
+    println!("{}", v);
+} else if from.as_str() == "L"
+    || from.as_str() == "gal"
+    || from.as_str() == "oz"
+    || from.as_str() == "되"
+{
+    let value = match from.as_str() {
+        "L" => volume::Volume::Liter(value),
+        "gal" => volume::Volume::Gallon(value),
+        "oz" => volume::Volume::Ounce(value),
+        "되" => volume::Volume::Doe(value),
+        _ => panic!(),
+    };
 
-        let converted = match to.as_str() {
-            "L" => volume::conv_to_liter(value),
-            "gal" => volume::conv_to_gallon(value),
-            "oz" => volume::conv_to_ounce(value),
-            "되" => volume::conv_to_doe(value),
-            _ => panic!(),
-        };
+    let converted = match to.as_str() {
+        "L" => volume::conv_to_liter(value),
+        "gal" => volume::conv_to_gallon(value),
+        "oz" => volume::conv_to_ounce(value),
+        "되" => volume::conv_to_doe(value),
+        _ => panic!(),
+    };
 
-        let v = match converted {
-            volume::Volume::Liter(l) => l,
-            volume::Volume::Gallon(gal) => gal,
-            volume::Volume::Ounce(oz) => oz,
-            volume::Volume::Doe(doe) => doe,
-        };
+    let v = match converted {
+        volume::Volume::Liter(l) => l,
+        volume::Volume::Gallon(gal) => gal,
+        volume::Volume::Ounce(oz) => oz,
+        volume::Volume::Doe(doe) => doe,
+    };
 
-        println!("{}", v);
-    }
-    */
+    println!("{}", v);
+}
+*/
--- src/unit.rs
+++ src/unit.rs
@@ -4,8 +4,68 @@ pub enum Unit {
     Volume,
 }
 
-pub fn identify(strings: &(String, String, String)) -> (Unit, Unit, ()) {
-    unimplemented!();
+fn from_str(s: &str) -> Unit {
+    if is_length_unit(s) {
+        Unit::Length
+    } else if is_area_unit(s) {
+        Unit::Area
+    } else if is_volume_unit(s) {
+        Unit::Volume
+    } else {
+        panic!()
+    }
+}
+
+const LENGTH_UNITS: [&'static str; 8] = ["미터", "m", "야드", "yd", "리", "里", "해리", "海里"];
+fn is_length_unit(s: &str) -> bool {
+    match LENGTH_UNITS.iter().position(|&u| u == s) {
+        Some(_) => true,
+        None => false,
+    }
+}
+
+const AREA_UNITS: [&'static str; 9] = [
+    "제곱미터",
+    "m^2",
+    "m²",
+    "m2",
+    "헥타르",
+    "ha",
+    "에이커",
+    "ac",
+    "평",
+];
+fn is_area_unit(s: &str) -> bool {
+    match AREA_UNITS.iter().position(|&u| u == s) {
+        Some(_) => true,
+        None => false,
+    }
+}
+
+const VOLUME_UNITS: [&'static str; 6] = ["리터", "L", "갤런", "gal", "온스", "되"];
+fn is_volume_unit(s: &str) -> bool {
+    match VOLUME_UNITS.iter().position(|&u| u == s) {
+        Some(_) => true,
+        None => false,
+    }
+}
+
+pub fn identify(strings: &(String, String, String)) -> (Unit, Unit, f64) {
+    if let Ok(val) = strings.0.parse::<f64>() {
+        (
+            from_str(strings.1.as_str()),
+            from_str(strings.2.as_str()),
+            val,
+        )
+    } else if let Ok(val) = strings.2.parse::<f64>() {
+        (
+            from_str(strings.0.as_str()),
+            from_str(strings.1.as_str()),
+            val,
+        )
+    } else {
+        panic!();
+    }
 }
 
 pub fn is_convertable(from: &Unit, to: &Unit) -> bool {
@@ -17,6 +77,6 @@ pub fn is_convertable(from: &Unit, to: &Unit) -> bool {
     }
 }
 
-pub fn convert(from: &Unit, to: &Unit, value: ()) -> f64 {
+pub fn convert(from: &Unit, to: &Unit, value: f64) -> f64 {
     unimplemented!();
 }
