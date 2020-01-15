f33158f31ab19c5469fcc844785c00a3b493153c
Tetramad <cbwjd777@gmail.com>
2020-01-15 19:34:00 +0900

    feat: 문장열은 단위로 변환하게 구현

--- src/unit.rs
+++ src/unit.rs
@@ -1,52 +1,74 @@
+use crate::area;
+use crate::length;
+use crate::volume;
+
 pub enum Unit {
-    Area,
-    Length,
-    Volume,
+    Length(length::Length),
+    Area(area::Area),
+    Volume(volume::Volume),
 }
 
 fn from_str(s: &str) -> Unit {
-    if is_length_unit(s) {
-        Unit::Length
-    } else if is_area_unit(s) {
-        Unit::Area
-    } else if is_volume_unit(s) {
-        Unit::Volume
+    if let Some(u) = to_length(s) {
+        Unit::Length(u)
+    } else if let Some(u) = to_area(s) {
+        Unit::Area(u)
+    } else if let Some(u) = to_volume(s) {
+        Unit::Volume(u)
     } else {
         panic!()
     }
 }
 
-const LENGTH_UNITS: [&'static str; 8] = ["미터", "m", "야드", "yd", "리", "里", "해리", "海里"];
-fn is_length_unit(s: &str) -> bool {
-    match LENGTH_UNITS.iter().position(|&u| u == s) {
-        Some(_) => true,
-        None => false,
+const METER_UNITS: [&'static str; 2] = ["미터", "m"];
+const YARD_UNITS: [&'static str; 2] = ["야드", "yd"];
+const RI_UNITS: [&'static str; 2] = ["리", "里"];
+const HAERI_UNITS: [&'static str; 2] = ["해리", "海里"];
+fn to_length(s: &str) -> Option<length::Length> {
+    if let Some(_) = METER_UNITS.iter().position(|&u| u == s) {
+        Some(length::Length::Meter(0.0))
+    } else if let Some(_) = YARD_UNITS.iter().position(|&u| u == s) {
+        Some(length::Length::Yard(0.0))
+    } else if let Some(_) = RI_UNITS.iter().position(|&u| u == s) {
+        Some(length::Length::Ri(0.0))
+    } else {
+        None
     }
 }
 
-const AREA_UNITS: [&'static str; 9] = [
-    "제곱미터",
-    "m^2",
-    "m²",
-    "m2",
-    "헥타르",
-    "ha",
-    "에이커",
-    "ac",
-    "평",
-];
-fn is_area_unit(s: &str) -> bool {
-    match AREA_UNITS.iter().position(|&u| u == s) {
-        Some(_) => true,
-        None => false,
+const SQRMETER_UNITS: [&'static str; 4] = ["제곱미터", "m^2", "m²", "m2"];
+const HACTARE_UNITS: [&'static str; 2] = ["헥타르", "ha"];
+const ACRE_UNITS: [&'static str; 2] = ["에이커", "ac"];
+const PYEONG_UNITS: [&'static str; 1] = ["평"];
+fn to_area(s: &str) -> Option<area::Area> {
+    if let Some(_) = SQRMETER_UNITS.iter().position(|&u| u == s) {
+        Some(area::Area::SqrMeter(0.0))
+    } else if let Some(_) = HACTARE_UNITS.iter().position(|&u| u == s) {
+        Some(area::Area::Hactare(0.0))
+    } else if let Some(_) = ACRE_UNITS.iter().position(|&u| u == s) {
+        Some(area::Area::Acre(0.0))
+    } else if let Some(_) = PYEONG_UNITS.iter().position(|&u| u == s) {
+        Some(area::Area::Pyeong(0.0))
+    } else {
+        None
     }
 }
 
-const VOLUME_UNITS: [&'static str; 6] = ["리터", "L", "갤런", "gal", "온스", "되"];
-fn is_volume_unit(s: &str) -> bool {
-    match VOLUME_UNITS.iter().position(|&u| u == s) {
-        Some(_) => true,
-        None => false,
+const LITER_UNITS: [&'static str; 2] = ["리터", "L"];
+const GALLON_UNITS: [&'static str; 2] = ["갤런", "gal"];
+const OUNCE_UNITS: [&'static str; 2] = ["온스", "oz"];
+const DOE_UNITS: [&'static str; 1] = ["되"];
+fn to_volume(s: &str) -> Option<volume::Volume> {
+    if let Some(_) = LITER_UNITS.iter().position(|&u| u == s) {
+        Some(volume::Volume::Liter(0.0))
+    } else if let Some(_) = GALLON_UNITS.iter().position(|&u| u == s) {
+        Some(volume::Volume::Gallon(0.0))
+    } else if let Some(_) = OUNCE_UNITS.iter().position(|&u| u == s) {
+        Some(volume::Volume::Ounce(0.0))
+    } else if let Some(_) = DOE_UNITS.iter().position(|&u| u == s) {
+        Some(volume::Volume::Doe(0.0))
+    } else {
+        None
     }
 }
 
@@ -70,9 +92,9 @@ pub fn identify(strings: &(String, String, String)) -> (Unit, Unit, f64) {
 
 pub fn is_convertable(from: &Unit, to: &Unit) -> bool {
     match (from, to) {
-        (Unit::Area, Unit::Area) | (Unit::Length, Unit::Length) | (Unit::Volume, Unit::Volume) => {
-            true
-        }
+        (Unit::Area(_), Unit::Area(_))
+        | (Unit::Length(_), Unit::Length(_))
+        | (Unit::Volume(_), Unit::Volume(_)) => true,
         _ => false,
     }
 }
