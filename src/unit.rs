d7370a295cc4ea69527b476acd4f84305c536af2
Tetramad <cbwjd777@gmail.com>
2020-01-15 20:07:04 +0900

    feat: 단위 변환 구현

--- src/unit.rs
+++ src/unit.rs
@@ -23,7 +23,6 @@ fn from_str(s: &str) -> Unit {
 const METER_UNITS: [&'static str; 2] = ["미터", "m"];
 const YARD_UNITS: [&'static str; 2] = ["야드", "yd"];
 const RI_UNITS: [&'static str; 2] = ["리", "里"];
-const HAERI_UNITS: [&'static str; 2] = ["해리", "海里"];
 fn to_length(s: &str) -> Option<length::Length> {
     if let Some(_) = METER_UNITS.iter().position(|&u| u == s) {
         Some(length::Length::Meter(0.0))
@@ -100,5 +99,456 @@ pub fn is_convertable(from: &Unit, to: &Unit) -> bool {
 }
 
 pub fn convert(from: &Unit, to: &Unit, value: f64) -> f64 {
-    unimplemented!();
+    match from {
+        Unit::Length(f) => match to {
+            Unit::Length(t) => match f {
+                length::Length::Meter(_) => {
+                    let f = length::Length::Meter(value);
+
+                    match t {
+                        length::Length::Meter(_) => {
+                            let t = length::conv_to_meter(f);
+
+                            if let length::Length::Meter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        length::Length::Yard(_) => {
+                            let t = length::conv_to_yard(f);
+
+                            if let length::Length::Yard(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        length::Length::Ri(_) => {
+                            let t = length::conv_to_ri(f);
+
+                            if let length::Length::Ri(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+                length::Length::Yard(_) => {
+                    let f = length::Length::Yard(value);
+
+                    match t {
+                        length::Length::Meter(_) => {
+                            let t = length::conv_to_meter(f);
+
+                            if let length::Length::Meter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        length::Length::Yard(_) => {
+                            let t = length::conv_to_yard(f);
+
+                            if let length::Length::Yard(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        length::Length::Ri(_) => {
+                            let t = length::conv_to_ri(f);
+
+                            if let length::Length::Ri(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+                length::Length::Ri(_) => {
+                    let f = length::Length::Ri(value);
+
+                    match t {
+                        length::Length::Meter(_) => {
+                            let t = length::conv_to_meter(f);
+
+                            if let length::Length::Meter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        length::Length::Yard(_) => {
+                            let t = length::conv_to_yard(f);
+
+                            if let length::Length::Yard(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        length::Length::Ri(_) => {
+                            let t = length::conv_to_ri(f);
+
+                            if let length::Length::Ri(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+            },
+            _ => panic!(),
+        },
+        Unit::Area(f) => match to {
+            Unit::Area(t) => match f {
+                area::Area::SqrMeter(_) => {
+                    let f = area::Area::SqrMeter(value);
+
+                    match t {
+                        area::Area::SqrMeter(_) => {
+                            let t = area::conv_to_sqr_meter(f);
+
+                            if let area::Area::SqrMeter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Hactare(_) => {
+                            let t = area::conv_to_hactare(f);
+
+                            if let area::Area::Hactare(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Acre(_) => {
+                            let t = area::conv_to_acre(f);
+
+                            if let area::Area::Acre(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Pyeong(_) => {
+                            let t = area::conv_to_pyeong(f);
+
+                            if let area::Area::Pyeong(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+                area::Area::Hactare(_) => {
+                    let f = area::Area::Hactare(value);
+
+                    match t {
+                        area::Area::SqrMeter(_) => {
+                            let t = area::conv_to_sqr_meter(f);
+
+                            if let area::Area::SqrMeter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Hactare(_) => {
+                            let t = area::conv_to_hactare(f);
+
+                            if let area::Area::Hactare(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Acre(_) => {
+                            let t = area::conv_to_acre(f);
+
+                            if let area::Area::Acre(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Pyeong(_) => {
+                            let t = area::conv_to_pyeong(f);
+
+                            if let area::Area::Pyeong(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+                area::Area::Acre(_) => {
+                    let f = area::Area::Acre(value);
+
+                    match t {
+                        area::Area::SqrMeter(_) => {
+                            let t = area::conv_to_sqr_meter(f);
+
+                            if let area::Area::SqrMeter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Hactare(_) => {
+                            let t = area::conv_to_hactare(f);
+
+                            if let area::Area::Hactare(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Acre(_) => {
+                            let t = area::conv_to_acre(f);
+
+                            if let area::Area::Acre(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Pyeong(_) => {
+                            let t = area::conv_to_pyeong(f);
+
+                            if let area::Area::Pyeong(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+                area::Area::Pyeong(_) => {
+                    let f = area::Area::Pyeong(value);
+
+                    match t {
+                        area::Area::SqrMeter(_) => {
+                            let t = area::conv_to_sqr_meter(f);
+
+                            if let area::Area::SqrMeter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Hactare(_) => {
+                            let t = area::conv_to_hactare(f);
+
+                            if let area::Area::Hactare(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Acre(_) => {
+                            let t = area::conv_to_acre(f);
+
+                            if let area::Area::Acre(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        area::Area::Pyeong(_) => {
+                            let t = area::conv_to_pyeong(f);
+
+                            if let area::Area::Pyeong(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+            },
+            _ => panic!(),
+        },
+        Unit::Volume(f) => match to {
+            Unit::Volume(t) => match f {
+                volume::Volume::Liter(_) => {
+                    let f = volume::Volume::Liter(value);
+
+                    match t {
+                        volume::Volume::Liter(_) => {
+                            let t = volume::conv_to_liter(f);
+
+                            if let volume::Volume::Liter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Gallon(_) => {
+                            let t = volume::conv_to_gallon(f);
+
+                            if let volume::Volume::Gallon(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Ounce(_) => {
+                            let t = volume::conv_to_ounce(f);
+
+                            if let volume::Volume::Ounce(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Doe(_) => {
+                            let t = volume::conv_to_doe(f);
+
+                            if let volume::Volume::Doe(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+                volume::Volume::Gallon(_) => {
+                    let f = volume::Volume::Gallon(value);
+
+                    match t {
+                        volume::Volume::Liter(_) => {
+                            let t = volume::conv_to_liter(f);
+
+                            if let volume::Volume::Liter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Gallon(_) => {
+                            let t = volume::conv_to_gallon(f);
+
+                            if let volume::Volume::Gallon(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Ounce(_) => {
+                            let t = volume::conv_to_ounce(f);
+
+                            if let volume::Volume::Ounce(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Doe(_) => {
+                            let t = volume::conv_to_doe(f);
+
+                            if let volume::Volume::Doe(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+                volume::Volume::Ounce(_) => {
+                    let f = volume::Volume::Ounce(value);
+
+                    match t {
+                        volume::Volume::Liter(_) => {
+                            let t = volume::conv_to_liter(f);
+
+                            if let volume::Volume::Liter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Gallon(_) => {
+                            let t = volume::conv_to_gallon(f);
+
+                            if let volume::Volume::Gallon(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Ounce(_) => {
+                            let t = volume::conv_to_ounce(f);
+
+                            if let volume::Volume::Ounce(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Doe(_) => {
+                            let t = volume::conv_to_doe(f);
+
+                            if let volume::Volume::Doe(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+                volume::Volume::Doe(_) => {
+                    let f = volume::Volume::Doe(value);
+
+                    match t {
+                        volume::Volume::Liter(_) => {
+                            let t = volume::conv_to_liter(f);
+
+                            if let volume::Volume::Liter(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Gallon(_) => {
+                            let t = volume::conv_to_gallon(f);
+
+                            if let volume::Volume::Gallon(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Ounce(_) => {
+                            let t = volume::conv_to_ounce(f);
+
+                            if let volume::Volume::Ounce(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                        volume::Volume::Doe(_) => {
+                            let t = volume::conv_to_doe(f);
+
+                            if let volume::Volume::Doe(value) = t {
+                                value
+                            } else {
+                                panic!()
+                            }
+                        }
+                    }
+                }
+            },
+            _ => panic!(),
+        },
+    }
 }
